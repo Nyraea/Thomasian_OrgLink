6c3f9c6065baccb0a9411ab2cd4c6575c5ab791b
Nyraea <agraviador.markjearalds@gmail.com>
2023-12-11 00:10:49 +0800

    hotfix (AppDbContext to Identity)
    Changed AppDbContext to base from Identity Framework

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -3,10 +3,12 @@ using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Reflection.Emit;
 using static System.Net.WebRequestMethods;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace ThomasianOrglist.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext
     {
         public DbSet<Student> Students { get; set; }
         public DbSet<Organization> Organizations { get; set; }
